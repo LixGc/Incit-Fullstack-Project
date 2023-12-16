8aabf21035ebc9c6d94ef33fc2524ad42a848158
Felix Giancarlo <felixketiga@gmail.com>
2023-12-16 17:00:44 +0700

    fix: fix package and modules not installed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const googleClientId = ["YOUR_GOOLGE_CLIENT_ID"]
+const googleClientId = "87973030685-3mlll838f2nmrj2445nlidjo9v77btv2.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={googleClientId}>
