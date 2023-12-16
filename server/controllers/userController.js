a406679f9b805afabe574be027eff926013e090b
Felix Giancarlo <felixketiga@gmail.com>
2023-12-16 13:33:42 +0700

    feat: added redis to get profile and dashboard

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,48 +5,62 @@ const redis = require("../helpers/redis");
 class UserController {
   static async profile(req, res, next) {
     try {
-      const user = await User.findByPk(req.user.id, { attributes: ["username", "email", "verified"], exclude: ["password", "verificationLink"] });
-      res.json(user);
+      let userProfile = await redis.get(`userProfile:${req.user.id}`);
+      if (!userProfile) {
+        const user = await User.findByPk(req.user.id, { attributes: ["username", "email", "verified"], exclude: ["password", "verificationLink"] });
+        userProfile = user;
+        await redis.set(`userProfile:${req.user.id}`, JSON.stringify(user));
+      } else {
+        userProfile = JSON.parse(userProfile);
+      }
+      res.json(userProfile);
     } catch (error) {
       next(error);
     }
   }
   static async dashboard(req, res, next) {
     try {
-      const user = await User.findAll({ include: [UserHistory], attributes: { exclude: ["password", "verified", "verificationLink"] } });
-      const userLogoutData = user.reduce((acc, currentUser) => {
-        const logoutHistories = currentUser.UserHistories.filter((history) => {
-          return history.name === "logout" && history.UserId === req.user.id;
-        });
+      let userDashboard = await redis.get(`userDashboard`);
+      if (!userDashboard) {
+        const user = await User.findAll({ include: [UserHistory], attributes: { exclude: ["password", "verified", "verificationLink"] } });
+        const userLogoutData = user.reduce((acc, currentUser) => {
+          const logoutHistories = currentUser.UserHistories.filter((history) => {
+            return history.name === "logout" && history.UserId === req.user.id;
+          });
 
-        return acc.concat(logoutHistories);
-      }, []);
-      let userActiveSession = await User.findAll(
-        {
-          include: [
-            {
-              model: UserHistory,
-              where: { name: "activeSession", UserId: req.user.id },
-            },
-          ],
-        },
-        { exclude: ["password", "verified", "verificationLink"] }
-      );
-      const activeToday = activeSessionChecker(userActiveSession);
-      if (!activeToday) {
-        await UserHistory.create({ name: "activeSession", UserId: req.user.id });
+          return acc.concat(logoutHistories);
+        }, []);
+        let userActiveSession = await User.findAll(
+          {
+            include: [
+              {
+                model: UserHistory,
+                where: { name: "activeSession", UserId: req.user.id },
+              },
+            ],
+          },
+          { exclude: ["password", "verified", "verificationLink"] }
+        );
+        const activeToday = activeSessionChecker(userActiveSession);
+        if (!activeToday) {
+          await UserHistory.create({ name: "activeSession", UserId: req.user.id });
+        }
+        let todayActiveSession = countTodayActiveSessions(user);
+        let averageActiveSessionsLast7Days = countAverageActiveSessionsLast7Days(user);
+        averageActiveSessionsLast7Days = averageActiveSessionsLast7Days.toFixed(2);
+        let result = {
+          user,
+          totalUser: user.length,
+          todayActiveSession,
+          averageActiveSessionsLast7Days,
+          userLogoutData,
+        };
+        userDashboard = result;
+        await redis.set(`userDashboard`, JSON.stringify(result));
+      } else {
+        userDashboard = JSON.parse(userDashboard);
       }
-      let todayActiveSession = countTodayActiveSessions(user);
-      let averageActiveSessionsLast7Days = countAverageActiveSessionsLast7Days(user);
-      averageActiveSessionsLast7Days = averageActiveSessionsLast7Days.toFixed(2);
-      let result = {
-        user,
-        totalUser: user.length,
-        todayActiveSession,
-        averageActiveSessionsLast7Days,
-        userLogoutData,
-      };
-      res.json(result);
+      res.json(userDashboard);
     } catch (error) {
       next(error);
     }
