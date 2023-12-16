f283dcdc34de1d51caba9dc0ed5f413052fd15e9
Felix Giancarlo <felixketiga@gmail.com>
2023-12-16 16:48:18 +0700

    feat: added loader for loading page

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,5 +1,12 @@
+import Loading from '../Loading.json';
+import Lottie from 'lottie-react';
+
 export const Loader = () => {
   return (
-
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center backdrop-blur z-50">
+      <div className="w-1/2 h-4/5">
+        <Lottie loop autoplay speed="1" background="transparent" animationData={Loading} />
+      </div>
+    </div>
   );
 };
